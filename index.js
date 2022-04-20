d534816c84730be52500af3e728f0811695dac3d
Nolan Frame <frame.nolan@gmail.com>
2022-04-20 11:47:03 -0700

    finished working code

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const htmlPage =
-
-    `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <h1>${userName}</h1>
-        <h2>${location}</h2>
-        <p>${userBio}</p>
-        <a href="${linkedIn}">${linkedIn}</a>
-        <br>
-        <a href="${gitHub}">${gitHub}</a>
-    </body>
-    </html>`;
-
-
 inquirer
     .prompt([
         {
@@ -42,16 +21,54 @@ inquirer
         {
             type: 'input',
             name: 'linkedIn',
-            message: 'LinkedIn Profile: ',
+            message: 'Link to LinkedIn Profile: ',
         },
         {
             type: 'input',
             name: 'gitHub',
-            message: 'GitHub Profile: ',
+            message: 'GitHub Username: ',
         }
     ])
-    .then((response) =>
-        fs.appendFile('index.html', htmlPage, (error, data) => 
+    .then((response) => {
+        const userName = response.userName;
+        const location = response.location;
+        const userBio = response.userBio;
+        const linkedIn = response.linkedIn;
+        const gitHub = response.gitHub;
+
+        const htmlPage =
+            `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="style.css">
+                <title>Quick Bio</title>
+            </head>
+            <body>
+                <header>
+                    <h1>${userName}</h1>
+                </header>
+                <h2>Location - ${location}</h2>
+                <section class="container-section">
+                    <div class="p-container">
+                        <h4>About Me</h4>
+                        <p>${userBio}</p>
+                    </div>
+                    <div class="links-container">
+                        <p>LinkedIn Profile: <a href="${linkedIn}" target="_blank">${linkedIn}</a></p>
+                        <br>
+                        <p>GitHub Username: <a href="https://github.com/${gitHub} target="_blank"">${gitHub}</a></p>
+                    </div>
+                </section>
+            </body>
+            </html>`;
+
+        console.log(userName, location, userBio, linkedIn, gitHub)
+
+        fs.writeFile('index.html', htmlPage, (error, data) => 
             error ? console.error(error) : console.log(data)
         )
-    );
+    })
+    
